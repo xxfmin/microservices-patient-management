bdd6ef4a5e574af1eecf3548375bab7dd29adadf
Felipe Min <felipemin03@gmail.com>
2025-09-07 21:07:24 -0400

    finished project

---
+++ billing-service/target/generated-sources/protobuf/grpc-java/billing/BillingServiceGrpc.java
@@ -0,0 +1,293 @@
+package billing;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.68.1)",
+    comments = "Source: billing_service.proto")
+@io.grpc.stub.annotations.GrpcGenerated
+public final class BillingServiceGrpc {
+
+  private BillingServiceGrpc() {}
+
+  public static final java.lang.String SERVICE_NAME = "BillingService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<billing.BillingRequest,
+      billing.BillingResponse> getCreateBillingAccountMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "CreateBillingAccount",
+      requestType = billing.BillingRequest.class,
+      responseType = billing.BillingResponse.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<billing.BillingRequest,
+      billing.BillingResponse> getCreateBillingAccountMethod() {
+    io.grpc.MethodDescriptor<billing.BillingRequest, billing.BillingResponse> getCreateBillingAccountMethod;
+    if ((getCreateBillingAccountMethod = BillingServiceGrpc.getCreateBillingAccountMethod) == null) {
+      synchronized (BillingServiceGrpc.class) {
+        if ((getCreateBillingAccountMethod = BillingServiceGrpc.getCreateBillingAccountMethod) == null) {
+          BillingServiceGrpc.getCreateBillingAccountMethod = getCreateBillingAccountMethod =
+              io.grpc.MethodDescriptor.<billing.BillingRequest, billing.BillingResponse>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(SERVICE_NAME, "CreateBillingAccount"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  billing.BillingRequest.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  billing.BillingResponse.getDefaultInstance()))
+              .setSchemaDescriptor(new BillingServiceMethodDescriptorSupplier("CreateBillingAccount"))
+              .build();
+        }
+      }
+    }
+    return getCreateBillingAccountMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static BillingServiceStub newStub(io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<BillingServiceStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<BillingServiceStub>() {
+        @java.lang.Override
+        public BillingServiceStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new BillingServiceStub(channel, callOptions);
+        }
+      };
+    return BillingServiceStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static BillingServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<BillingServiceBlockingStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<BillingServiceBlockingStub>() {
+        @java.lang.Override
+        public BillingServiceBlockingStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new BillingServiceBlockingStub(channel, callOptions);
+        }
+      };
+    return BillingServiceBlockingStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static BillingServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<BillingServiceFutureStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<BillingServiceFutureStub>() {
+        @java.lang.Override
+        public BillingServiceFutureStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new BillingServiceFutureStub(channel, callOptions);
+        }
+      };
+    return BillingServiceFutureStub.newStub(factory, channel);
+  }
+
+  /**
+   */
+  public interface AsyncService {
+
+    /**
+     */
+    default void createBillingAccount(billing.BillingRequest request,
+        io.grpc.stub.StreamObserver<billing.BillingResponse> responseObserver) {
+      io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(getCreateBillingAccountMethod(), responseObserver);
+    }
+  }
+
+  /**
+   * Base class for the server implementation of the service BillingService.
+   */
+  public static abstract class BillingServiceImplBase
+      implements io.grpc.BindableService, AsyncService {
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return BillingServiceGrpc.bindService(this);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do asynchronous rpc calls to service BillingService.
+   */
+  public static final class BillingServiceStub
+      extends io.grpc.stub.AbstractAsyncStub<BillingServiceStub> {
+    private BillingServiceStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected BillingServiceStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new BillingServiceStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void createBillingAccount(billing.BillingRequest request,
+        io.grpc.stub.StreamObserver<billing.BillingResponse> responseObserver) {
+      io.grpc.stub.ClientCalls.asyncUnaryCall(
+          getChannel().newCall(getCreateBillingAccountMethod(), getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do synchronous rpc calls to service BillingService.
+   */
+  public static final class BillingServiceBlockingStub
+      extends io.grpc.stub.AbstractBlockingStub<BillingServiceBlockingStub> {
+    private BillingServiceBlockingStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected BillingServiceBlockingStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new BillingServiceBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public billing.BillingResponse createBillingAccount(billing.BillingRequest request) {
+      return io.grpc.stub.ClientCalls.blockingUnaryCall(
+          getChannel(), getCreateBillingAccountMethod(), getCallOptions(), request);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do ListenableFuture-style rpc calls to service BillingService.
+   */
+  public static final class BillingServiceFutureStub
+      extends io.grpc.stub.AbstractFutureStub<BillingServiceFutureStub> {
+    private BillingServiceFutureStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected BillingServiceFutureStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new BillingServiceFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<billing.BillingResponse> createBillingAccount(
+        billing.BillingRequest request) {
+      return io.grpc.stub.ClientCalls.futureUnaryCall(
+          getChannel().newCall(getCreateBillingAccountMethod(), getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_CREATE_BILLING_ACCOUNT = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final AsyncService serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(AsyncService serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_CREATE_BILLING_ACCOUNT:
+          serviceImpl.createBillingAccount((billing.BillingRequest) request,
+              (io.grpc.stub.StreamObserver<billing.BillingResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  public static final io.grpc.ServerServiceDefinition bindService(AsyncService service) {
+    return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+        .addMethod(
+          getCreateBillingAccountMethod(),
+          io.grpc.stub.ServerCalls.asyncUnaryCall(
+            new MethodHandlers<
+              billing.BillingRequest,
+              billing.BillingResponse>(
+                service, METHODID_CREATE_BILLING_ACCOUNT)))
+        .build();
+  }
+
+  private static abstract class BillingServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    BillingServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return billing.BillingServiceOuterClass.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("BillingService");
+    }
+  }
+
+  private static final class BillingServiceFileDescriptorSupplier
+      extends BillingServiceBaseDescriptorSupplier {
+    BillingServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class BillingServiceMethodDescriptorSupplier
+      extends BillingServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final java.lang.String methodName;
+
+    BillingServiceMethodDescriptorSupplier(java.lang.String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (BillingServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new BillingServiceFileDescriptorSupplier())
+              .addMethod(getCreateBillingAccountMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
